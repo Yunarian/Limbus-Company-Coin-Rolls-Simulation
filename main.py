457c97132be3ebec9b6c38f1d672ad500f9c44c3
Yunarian <167535928+Yunarian@users.noreply.github.com>
2024-05-14 08:46:28 -0400

    Add files via upload
    Changed coin_rolls function to take an integer and tuple -- planning make the clash_calculate function take two tuples and integers.

--- main.py
+++ main.py
@@ -19,33 +19,37 @@ run = True
 
 
 # Functions used in the pygame loop
-# Takes 4 integers as parameters. The first two generate which side of the coin was rolled.
+# Takes a tuple and an integer as parameters.
+# The first two elements of the tuple generate which side of the coin was rolled.
 # The next two generate what power the coins rolled should have.
-def coin_rolls(coins, sanity, coin_power, base_power):
+# skill_attributes = coins, coin_power, base_power
+def coin_rolls(sanity, skill_attributes):
     coin_side_roll = []
 
-    for i in range(coins):
+    for i in range(skill_attributes[0]):
         if random.randint(1, 100) <= 50 + sanity:
             coin_side_roll.append("Heads")
         else:
             coin_side_roll.append("Tails")
 
     attack_rolls = []
-    current_attack_power = base_power
+    current_attack_power = skill_attributes[2]
 
     for i in range(len(coin_side_roll)):
         if coin_side_roll[i] == "Heads":
-            current_attack_power += coin_power
+            current_attack_power += skill_attributes[1]
 
         attack_rolls.append(current_attack_power)
 
     return attack_rolls
 
 
+print(coin_rolls(0, (3, 4, 4)))
+
 # takes 8 integer parameters, with each side of the clash having 4 parameters
-# Print statements currently here to see if the function works (It does)
-def clash_calculate(ally_coins, ally_coin_power, ally_base_power, ally_sanity,
-                    enemy_coins, enemy_coin_power, enemy_base_power, enemy_sanity):
+# Gonna try to change this to take a tuple for the coins, coin power, and base power.
+def clash_calculate(ally_sanity, ally_coins, ally_coin_power, ally_base_power,
+                    enemy_sanity, enemy_coins, enemy_coin_power, enemy_base_power):
     damage_dealt = 0
 
     while ally_coins != 0 and enemy_coins != 0:
@@ -90,37 +94,54 @@ meursault = Meursault(150, 400)
 meursault_s1 = MeursaultSkill(25, 25, "S1")
 meursault_s2 = MeursaultSkill(25, 285, "S2")
 meursault_s3 = MeursaultSkill(25, 545, "S3")
+# first element is s1, second is s2, third is s3
+meursault_skill_description_render = [False, False, False]
 
 trash_crab = TrashCrab(800, 450)
 trash_crab_gwah = TrashCrabSkill(1075, 20, "Gwah")
 trash_crab_gwaaah = TrashCrabSkill(1075, 195, "Gwaaah")
 trash_crab_shell_tackle = TrashCrabSkill(1075, 370, "Shell Tackle")
 trash_crab_foaming = TrashCrabSkill(1075, 545, "Foaming")
+# first is gwah, second is gwaaah, third is shell tackle, fourth is foaming.
+trash_crab_skill_description_render = [False, False, False, False]
 
 # Plan: Have Meursault's three skills to the left of him, and the trash cab's skills to the right of it.
 # Clicking on the appropriate skill would have that skill be used, and the associated animation if the attack hits.
 
 # Pygame loop
 while run:
+
     for event in pygame.event.get():  # User did something
+
+        # Mouse hover-over detection with Meursault's skills
+        meursault_skill_description_render = [False, False, False]
+        if meursault_s1.rect.collidepoint(pygame.mouse.get_pos()):
+            meursault_skill_description_render[0] = True
+
+        if meursault_s2.rect.collidepoint(pygame.mouse.get_pos()):
+            meursault_skill_description_render[1] = True
+
+        if meursault_s3.rect.collidepoint(pygame.mouse.get_pos()):
+            meursault_skill_description_render[2] = True
+
+        # Mouse hover-over detection with trash crab skills
+        trash_crab_skill_description_render = [False, False, False, False]
+        if trash_crab_gwah.rect.collidepoint(pygame.mouse.get_pos()):
+            trash_crab_skill_description_render[0] = True
+
+        if trash_crab_gwaaah.rect.collidepoint(pygame.mouse.get_pos()):
+            trash_crab_skill_description_render[1] = True
+
+        if trash_crab_shell_tackle.rect.collidepoint(pygame.mouse.get_pos()):
+            trash_crab_skill_description_render[2] = True
+
+        if trash_crab_foaming.rect.collidepoint(pygame.mouse.get_pos()):
+            trash_crab_skill_description_render[3] = True
+
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                # Mouse click detection with meursault + his skills
-                if meursault.rect.collidepoint(event.pos):
-                    print("Meursault.")
-
-                if meursault_s1.rect.collidepoint(event.pos):
-                    print("S1")
-
-                if meursault_s2.rect.collidepoint(event.pos):
-                    print("S2")
-
-                if meursault_s3.rect.collidepoint(event.pos):
-                    print("S3")
-
-                # Mouse click detection with trash crab + their skills
-                if trash_crab.rect.collidepoint(event.pos):
-                    print("Meursault.")
+            if event.button == 1:  # Mouse click detection with meursault + his skills
+                print(meursault_skill_description_render)
+                print(trash_crab_skill_description_render)
 
         if event.type == pygame.QUIT:  # If user clicked close
             run = False
